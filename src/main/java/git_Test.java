e06379afe4c659b6d72fef125cfbd42d69c332a2
0826 <0826@atguigu.com>
2021-12-29 19:43:29 +0800

    hot-fix commit

--- src/main/java/git_Test.java
+++ src/main/java/git_Test.java
@@ -2,5 +2,6 @@ public class git_Test {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("aaa");
+        System.out.println("hot-fix");
     }
 }
